05b480d881c21e1a85a97385befece1075e6fa35
Tung Nguyen <tungshooter@gmail.com>
2017-03-31 18:16:59 +0700

    Components

--- main.js
+++ main.js
@@ -43,24 +43,169 @@
 // });
 
 // Computed Properties
-let app = new Vue({
-    el: '#root',
-    data: {
-        tasks: [
-            { description: '1 Task', completed: true },
-            { description: '2 Task', completed: false },
-            { description: '3 Task', completed: false },
-            { description: '4 Task', completed: false },
-            { description: '5 Task', completed: false },
-            { description: '6 Task', completed: true },
-        ]
+// let app = new Vue({
+//     el: '#root',
+//     data: {
+//         tasks: [
+//             { description: '1 Task', completed: true },
+//             { description: '2 Task', completed: false },
+//             { description: '3 Task', completed: false },
+//             { description: '4 Task', completed: false },
+//             { description: '5 Task', completed: false },
+//             { description: '6 Task', completed: true },
+//         ]
+//     },
+//     computed: {
+//         incompletedTasks() {
+//             return this.tasks.filter(task => ! task.completed)
+//         },
+//         completedTasks() {
+//             return this.tasks.filter(task => task.completed)
+//         }
+//     }
+// })
+
+// Components
+// Vue.component('task-list', {
+//     template: `
+//         <div>
+//             <task v-for="task in tasks">{{ task.description }}</task>
+//         </div>
+//     `,
+//     data() {
+//         return {
+//             tasks: [
+//                 { description: '1 Task', completed: true },
+//                 { description: '2 Task', completed: false },
+//                 { description: '3 Task', completed: false },
+//                 { description: '4 Task', completed: false },
+//                 { description: '5 Task', completed: false },
+//                 { description: '6 Task', completed: true },
+//             ]
+//         }
+//     }
+// })
+// Vue.component('task', {
+//     template: '<li><slot></slot></li>'
+// })
+// new Vue({
+//     el: '#root'
+// })
+
+// Message Component
+// Vue.component('message', {
+//     props: ['title', 'body'],
+//     data() {
+//         return {
+//             isShow: true
+//         }
+//     },
+//     template: `
+//         <article class="message is-primary" v-show="isShow">
+//             <div class="message-header">
+//                 <p>{{ title }}</p>
+//                 <button class="delete" @click="hideMessage"></button>
+//             </div>
+//             <div class="message-body">
+//                 {{ body }}
+//             </div>
+//         </article>
+//     `,
+//     methods: {
+//         hideMessage() {
+//             this.isShow = false;
+//         }
+//     }
+// })
+// new Vue({
+//     el: '#root'
+// })
+
+
+// Modal Component
+// Vue.component('modal', {
+//     template: `
+//         <div class="modal is-active">
+//             <div class="modal-background"></div>
+//             <div class="modal-content">
+//                 <div class="box">
+//                     <slot></slot>
+//                 </div>
+//             </div>
+//             <button class="modal-close" @click="$emit('close')"></button>
+//         </div>
+//     `
+// })
+// new Vue({
+//     el: '#root',
+//     data: {
+//         isShow: false
+//     }
+// })
+
+Vue.component('tabs', {
+    template: `
+        <div>
+            <div class="tabs">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                        <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="tabs-details">
+                <slot></slot>
+            </div>
+        </div>
+    `,
+
+    created() {
+        this.tabs = this.$children
     },
-    computed: {
-        incompletedTasks() {
-            return this.tasks.filter(task => ! task.completed)
-        },
-        completedTasks() {
-            return this.tasks.filter(task => task.completed)
+
+    data() {
+        return {
+            tabs: []
+        }
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name)
+            })
         }
     }
+})
+
+Vue.component('tab', {
+    template: `
+        <div v-show="isActive"><slot></slot></div>
+    `,
+
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+
+    data() {
+        return {
+            isActive: false
+        }
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-')
+        }
+    },
+
+    mounted() {
+        this.isActive = this.selected
+    }
+})
+
+new Vue({
+    el: '#root'
 })
